eef86d496ea9541b1ee75abd2202c3a2fb9f9e8d
Emeka Mosanya <emeka@mosanya.net>
2013-05-15 15:05:56 +0100

    Changed the install script to my needs.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 gradle clean install zip
-rm -dfr /java/tools/benchy-0.1-SNAPSHOT/
-unzip -o build/benchy-0.1-SNAPSHOT.zip -d /java/tools/
-chmod +x /java/tools/benchy-0.1-SNAPSHOT/bin/*
-cp benchy-driver/build/libs/* /java/projects/multiverse/multiverse-beta/lib/
+rm -dfr ~/work/tools/benchy-0.1-SNAPSHOT/
+unzip -o build/benchy-0.1-SNAPSHOT.zip -d ~/work/tools/
+chmod +x ~/work/tools/benchy-0.1-SNAPSHOT/bin/*
+#cp benchy-driver/build/libs/* /java/projects/multiverse/multiverse-beta/lib/
